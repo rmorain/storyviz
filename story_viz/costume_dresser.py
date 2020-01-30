90566490b7dfcbaa36b6404348f57efbc6a8fedc
Connor Wilhelm <wilhelmconnorr@gmail.com>
2020-01-30 16:59:16 -0700

    Implemented CostumeDresser with simple functionality

--- story_viz/costume_dresser.py
+++ story_viz/costume_dresser.py
@@ -3,13 +3,28 @@ Maps keywords provided by story interpreter to specific schematics.
 Returns list of StorySchematic objects 
 """
 
-
-
+import os
+import random
 from classes import StorySchematics
 
 class CostumeDresser:
     def __init__(self):
-        pass
+        self.__PATH__TO__SCHEMATICS = "../stock-schematics/library/"
+        self.__FILE__TYPE = ".schematic"
 
-    def get_schematics(keywords):
-        return [StorySchematic()]
+    def get_schematics(self, keywords):
+        schematic_names = os.listdir(self.__PATH__TO__SCHEMATICS)
+        found_schematics = []
+        for keyword in keywords:
+            keyword = keyword.lower()
+            # for now, it shuffles the schematic names to add some randomness
+            random.shuffle(schematic_names)
+            found_schematic = None
+            for schem in schematic_names:
+                if keyword in schem:
+                    # Remove the file ending, since StorySchematics adds it on later
+                    found_schematic = schem.replace(self.__FILE__TYPE, "")
+                    break
+            # What to do if keyword is not found?
+            found_schematics.append(found_schematic)
+        return [StorySchematics(keywords, found_schematics)] # takes in array of labels and array of schematics
