4443c970092197edb8d6b20bf81c69f0f83fa7bb
Rob Morain <robert.morain@gmail.com>
2020-01-31 13:33:33 -0700

    Use rake for keyword extraction. Wrote test

--- story_viz/story_interpreter.py
+++ story_viz/story_interpreter.py
@@ -4,11 +4,14 @@ Return list of tuples.
 Tuples are associated keywords.
 """
 
+from rake_nltk import Rake
 
 
 class StoryInterpreter:
     def __init__(self):
-        pass
+        self.r = Rake()
 
-    def get_keywords(story):
-        return [()]
+    def get_keywords(self, story):
+        print(story)
+        keywords_list = self.r.extract_keywords_from_text(story)
+        return r.get_ranked_phrases()
---
+++ story_viz/tests/story_interpreter_test.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.append(os.getcwd() + '/..')
+print(sys.path)
+
+from story_interpreter import StoryInterpreter
+
+s = StoryInterpreter()
+
+story = "Once upon a time, there was a gay little fish named Reed. He was very sad and he lived underwater. Then one day he had a million friends and he was happy. The End."
+
+s.get_keywords(story)
