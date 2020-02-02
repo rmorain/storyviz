5739b9ec7e9656e4cbde8fe60b944bde081abfce
demkejon001 <demkejon001@gmail.com>
2020-02-02 09:48:18 -0700

    Changed BuildFence to have the correct coordinates and now fences are built.

--- story_viz/general_schematic_placer.py
+++ story_viz/general_schematic_placer.py
@@ -30,82 +30,16 @@ class GeneralSchematicPlacer:
         max_width, max_length = land_allocation_grid.shape
         for width in range(max_width//house_size[0]):
             for length in range(max_length//house_size[1]):
-                print(max_width//house_size[0], max_length//house_size[1])
-                print(width, length)
                 yard = land_allocation_grid[width*house_size[0]:width*house_size[0]+house_size[0],
                                             length*house_size[1]:length*house_size[1]+house_size[1]]
                 if not np.any(yard==1):
                     yard_box = BoundingBox((box.minx+width*house_size[0], box.miny, box.minz+length*house_size[1]),
-                                (house_size[0], box.maxy, house_size[1]))
-                    buildFence(level, yard_box)
+                                           (house_size[0], box.maxy, house_size[1]))
                     yard_boxes.append(yard_box)
                 land_allocation_grid[width*house_size[0], length*house_size[1]] = 1
+
         for yard_box in yard_boxes:
-            print('yardbox', yard_box)
-        print('box', box)
-        #print(land_allocation_grid)
-
-#splits the given box into 4 unequal areas
-def binaryPartition(box):
-    partitions = []
-    # create a queue which holds the next areas to be partitioned
-    queue = []
-    queue.append(box)
-    # for as long as the queue still has boxes to partition...
-    count = 0
-    while len(queue) > 0:
-        count += 1
-        splitMe = queue.pop(0)
-        (width, height, depth) = utilityFunctions.getBoxSize(splitMe)
-        # print "Current partition width,depth",width,depth
-        centre = 0
-        # this bool lets me know which dimension I will be splitting on. It matters when we create the new outer bound size
-        isWidth = False
-        # find the larger dimension and divide in half
-        # if the larger dimension is < 10, then block this from being partitioned
-        minSize = 12
-        if width > depth:
-            # roll a random die, 1% change we stop anyways
-            chance = random.randint(100)
-
-            if depth < minSize or chance == 1:
-                partitions.append(splitMe)
-                continue
-
-            isWidth = True
-            centre = width / 2
-        else:
-            chance = random.randint(10)
-            if width < minSize or chance == 1:
-                partitions.append(splitMe)
-                continue
-            centre = depth / 2
-
-        # a random modifier for binary splitting which is somewhere between 0 and 1/16 the total box side length
-        randomPartition = random.randint(0, (centre / 8) + 1)
-
-        # creating the new bound
-        newBound = centre + randomPartition
-
-        #creating the outer edge bounds
-        outsideNewBounds = 0
-        if isWidth:
-            outsideNewBound = width - newBound - 1
-        else:
-            outsideNewBound = depth - newBound - 1
-
-        # creating the bounding boxes
-        # NOTE: BoundingBoxes are objects contained within pymclevel and can be instantiated as follows
-        # BoundingBox((x,y,z), (sizex, sizey, sizez))
-        # in this instance, you specifiy which corner to start, and then the size of the box dimensions
-        # this is an if statement to separate out binary partitions by dimension (x and z)
-        if isWidth:
-            queue.append(BoundingBox((splitMe.minx, splitMe.miny, splitMe.minz), (newBound-1, 256, depth)))
-            queue.append(BoundingBox((splitMe.minx + newBound + 1, splitMe.miny, splitMe.minz), (outsideNewBound - 1, 256, depth)))
-        else:
-            queue.append(BoundingBox((splitMe.minx, splitMe.miny, splitMe.minz), (width, 256, newBound - 1)))
-            queue.append(BoundingBox((splitMe.minx, splitMe.miny, splitMe.minz + newBound + 1), (width, 256, outsideNewBound - 1)))
-    return partitions
+            buildFence(level, yard_box)
 
 # builds a wooden fence around the perimeter of this box, like this photo
 #			  Top - zmax
@@ -126,39 +60,37 @@ def buildFence(level, box):
 
     # add top fence blocks
     for x in range(box.minx, box.maxx):
-        for y in xrange(box.maxy, box.miny, -1):
+        for z in xrange(box.maxz, box.minz, -1):
                 # get this block
-                tempBlock = level.blockAt(x, y, box.maxz)
-                print(x, y, box.maxz)
-                print(tempBlock)
+                tempBlock = level.blockAt(x, box.miny, box.maxz)
                 if tempBlock != 0:
                     newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), x, y+1, box.maxz)
+                    utilityFunctions.setBlock(level, (85, newValue), x, box.miny+1, box.maxz)
                     break;
     # add bottom fence blocks (don't double count corner)
     for x in range(box.minx, box.maxx):
-        for y in xrange(box.maxy, box.miny, -1):
+        for z in xrange(box.maxz, box.minz, -1):
                 # get this block
-                tempBlock = level.blockAt(x, y, box.minz)
+                tempBlock = level.blockAt(x, box.miny, box.minz)
                 if tempBlock != 0:
                     newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), x, y+1, box.minz)
+                    utilityFunctions.setBlock(level, (85, newValue), x, box.miny+1, box.minz)
                     break;
     # add left fence blocks (don't double count corner)
     for z in range(box.minz+1, box.maxz):
-        for y in xrange(box.maxy, box.miny, -1):
+        for x in xrange(box.maxx, box.minx, -1):
                 # get this block
-                tempBlock = level.blockAt(box.minx, y, z)
+                tempBlock = level.blockAt(box.minx, box.miny, z)
                 if tempBlock != 0:
                     newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), box.minx, y+1, z)
+                    utilityFunctions.setBlock(level, (85, newValue), box.minx, box.miny+1, z)
                     break;
     # add right fence blocks
     for z in range(box.minz, box.maxz+1):
-        for y in xrange(box.maxy, box.miny, -1):
+        for x in xrange(box.maxx, box.minx, -1):
                 # get this block
-                tempBlock = level.blockAt(box.maxx, y, z)
+                tempBlock = level.blockAt(box.maxx, box.miny, z)
                 if tempBlock != 0:
                     newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), box.maxx, y+1, z)
+                    utilityFunctions.setBlock(level, (85, newValue), box.maxx, box.miny+1, z)
                     break;
