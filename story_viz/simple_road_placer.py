4ea9d76247a5501fcfc85c527f12ba8a3bc893f0
demkejon001 <demkejon001@gmail.com>
2020-02-11 18:20:06 -0700

    simple_road_placer.py now builds road along x axis

--- story_viz/simple_road_placer.py
+++ story_viz/simple_road_placer.py
@@ -16,10 +16,14 @@ class SimpleRoadPlacer:
     def __init__(self):
         pass
 
+    # box.width = x
+    # box.length = z
     def place(self, level, box, options):
-        road_placement = np.zeros((box.width, box.length))
-        x, y = road_placement.shape
-        for i in range(y):
-            road_placement[x//2][i] = 1
-            utilityFunctions.setBlock(level, (1, 0), box.minx+(x//2), box.miny, box.minz+y)
+        road_placement = np.zeros((box.length, box.width))
+        z, x = road_placement.shape
+        for offset_x in range(x):
+            road_placement[z//2][offset_x] = 1
+            utilityFunctions.setBlock(level, (1, 0), box.minx+offset_x, box.miny, box.minz+(z//2))
+        utilityFunctions.setBlock(level, (1, 0), box.minx, box.miny, box.minz)
+        print(road_placement)
         return road_placement
