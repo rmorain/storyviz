d95ac664c4b023dfcbaf77819ba53ab1112c783d
demkejon001 <demkejon001@gmail.com>
2020-02-19 19:27:01 -0700

    Simplified buildYard code and add code to build roads between houses

--- story_viz/general_schematic_placer.py
+++ story_viz/general_schematic_placer.py
@@ -26,15 +26,16 @@ class GeneralSchematicPlacer:
         yards = self.get_yards(land_allocation_grid, level, box)
         for yard in yards:
             buildFence(level, yard)
+            buildRoads(level, yard)
             #m.copyBlocksFrom(schematics['house'], level, yard, )
             source_box = BoundingBox((0,0,0), (schematics['house'].Width,schematics['house'].Height,schematics['house'].Length))
-            level.copyBlocksFrom(schematics['house'], source_box, (yard.minx+1, yard.miny-5, yard.minz+1))
+            level.copyBlocksFrom(schematics['house'], source_box, (yard.minx+2, yard.miny-5, yard.minz+2))
             # TODO: We are using yard.miny-5 because some schematics come with their own grass which way have to deal with.
 
 
     def get_yards(self, land_allocation_grid, level, box):
         yard_boxes = []
-        house_size = (15, 15)
+        house_size = (20, 20)
         max_width, max_length = land_allocation_grid.shape
         for width in range(max_width//house_size[0]):
             for length in range(max_length//house_size[1]):
@@ -59,44 +60,59 @@ class GeneralSchematicPlacer:
 #       |              |
 #       ----------------
 #			Bottom - zmin
+def buildRoads(level, box):
+    for x in range(box.minx, box.maxx):
+        utilityFunctions.setBlock(level, (1, 0), x, box.miny, box.maxz)
+        utilityFunctions.setBlock(level, (1, 0), x, box.miny, box.minz)
+    for z in range(box.minz, box.maxz):
+        utilityFunctions.setBlock(level, (1, 0), box.maxx, box.miny, z)
+        utilityFunctions.setBlock(level, (1, 0), box.minx, box.miny, z)
+
 def buildFence(level, box):
 
     # side by side, go row/column by row/column, and move down the pillar in the y axis starting from the top
     # look for the first non-air tile (id != 0). The tile above this will be a fence tile
 
-    # add top fence blocks
-    for x in range(box.minx, box.maxx):
-        for z in xrange(box.maxz, box.minz, -1):
-                # get this block
-                tempBlock = level.blockAt(x, box.miny, box.maxz)
-                if tempBlock != 0:
-                    newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), x, box.miny+1, box.maxz)
-                    break;
-    # add bottom fence blocks (don't double count corner)
-    for x in range(box.minx, box.maxx):
-        for z in xrange(box.maxz, box.minz, -1):
-                # get this block
-                tempBlock = level.blockAt(x, box.miny, box.minz)
-                if tempBlock != 0:
-                    newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), x, box.miny+1, box.minz)
-                    break;
-    # add left fence blocks (don't double count corner)
-    for z in range(box.minz+1, box.maxz):
-        for x in xrange(box.maxx, box.minx, -1):
-                # get this block
-                tempBlock = level.blockAt(box.minx, box.miny, z)
-                if tempBlock != 0:
-                    newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), box.minx, box.miny+1, z)
-                    break;
-    # add right fence blocks
-    for z in range(box.minz, box.maxz+1):
-        for x in xrange(box.maxx, box.minx, -1):
-                # get this block
-                tempBlock = level.blockAt(box.maxx, box.miny, z)
-                if tempBlock != 0:
-                    newValue = 0
-                    utilityFunctions.setBlock(level, (85, newValue), box.maxx, box.miny+1, z)
-                    break;
+    for x in range(box.minx+1, box.maxx-1):
+        utilityFunctions.setBlock(level, (85, 0), x, box.miny+1, box.maxz-1)
+        utilityFunctions.setBlock(level, (85, 0), x, box.miny+1, box.minz+1)
+    for z in range(box.minz+1, box.maxz-1):
+        utilityFunctions.setBlock(level, (85, 0), box.maxx-1, box.miny + 1, z)
+        utilityFunctions.setBlock(level, (85, 0), box.minx+1, box.miny + 1, z)
+
+    # # add top fence blocks
+    # for x in range(box.minx, box.maxx):
+    #     for z in xrange(box.maxz-1, box.minz+1, -1):
+    #             # get this block
+    #             tempBlock = level.blockAt(x, box.miny, box.maxz)
+    #             if tempBlock != 0:
+    #                 newValue = 0
+    #                 utilityFunctions.setBlock(level, (85, newValue), x, box.miny+1, box.maxz)
+    #                 break;
+    # # add bottom fence blocks (don't double count corner)
+    # for x in range(box.minx, box.maxx):
+    #     for z in xrange(box.maxz, box.minz+1, -1):
+    #             # get this block
+    #             tempBlock = level.blockAt(x, box.miny, box.minz)
+    #             if tempBlock != 0:
+    #                 newValue = 0
+    #                 utilityFunctions.setBlock(level, (85, newValue), x, box.miny+1, box.minz)
+    #                 break;
+    # # add left fence blocks (don't double count corner)
+    # for z in range(box.minz+1, box.maxz):
+    #     for x in xrange(box.maxx, box.minx, -1):
+    #             # get this block
+    #             tempBlock = level.blockAt(box.minx, box.miny, z)
+    #             if tempBlock != 0:
+    #                 newValue = 0
+    #                 utilityFunctions.setBlock(level, (85, newValue), box.minx, box.miny+1, z)
+    #                 break;
+    # # add right fence blocks
+    # for z in range(box.minz, box.maxz+1):
+    #     for x in xrange(box.maxx, box.minx, -1):
+    #             # get this block
+    #             tempBlock = level.blockAt(box.maxx, box.miny, z)
+    #             if tempBlock != 0:
+    #                 newValue = 0
+    #                 utilityFunctions.setBlock(level, (85, newValue), box.maxx, box.miny+1, z)
+    #                 break;
