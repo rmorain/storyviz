b7883e35025aa7e5baeecac9d528977b5da26d27
demkejon001 <demkejon001@gmail.com>
2020-02-20 20:17:47 -0700

    Added LJ_potential (attraction-repulsion) and balance functions

--- story_viz/EmilienGeneration_prac.py
+++ story_viz/EmilienGeneration_prac.py
@@ -3,6 +3,12 @@ import random
 import matplotlib.pyplot as plt
 from scipy.stats import skewnorm
 
+class Building: # Should this be a namedtuple ???
+    def __init__(self, type):
+        self.type = "house"
+        self.position = (0,0)
+
+
 def get_coord_offset(coord, offset, scale):
     scaled_offset = tuple(scale*x for x in offset)
     return tuple(coord[i]+scaled_offset[i] for i in range(len(coord)))
@@ -30,14 +36,51 @@ def generate_hill(terrain, peak_max):
                 point = (point[0] + dz, point[1] + dx)
                 modify_terrain(terrain, point, terrain_height - distance)
 
+def get_local_interest():
+    pass
+
+def generate_building(terrain, village_skeleton, building):
+    z, x = terrain.shape
+    coord = random.randint(0, z-1), random.randint(0, x-1)
+    get_local_interest(terrain, village_skeleton, building, coord)
+
+def close_distance():
+    pass
+
+def balance(x):
+    a = 3
+    loc = 0
+    scale = .33
+    rv = skewnorm(a, loc, scale)
+    return rv.pdf(x)
+
+# Attraction - Repulsion
+def LJ_potential(r):
+    sigma = 1.03
+    e = 1
+    return -4*e*((sigma/r)**12 - (sigma/r)**6)
+
 def main():
     terrain = np.zeros((100,100))
     for _ in range(random.randint(1,5)):
         generate_hill(terrain, 30)
     #print(terrain)
-    plt.imshow(terrain, cmap='hot', interpolation='nearest')
+    # plt.imshow(terrain, cmap='hot', interpolation='nearest')
 
-    plt.show()
+    # Attraction-Repulsion Interest Function Graph
+    # x = np.linspace(1,3,1000)
+    # y = [LJ_potential(r) for r in x]
+    # plt.plot(x, y)
+    # plt.show()
+
+    # Balance
+    # x = np.linspace(-.25, 1.25, 1000)
+    # y = [balance(i) for i in x]
+    # plt.plot(x, y)
+    # plt.show()
+
+
+    village_skeleton = []
 
 if __name__=='__main__':
     main()
