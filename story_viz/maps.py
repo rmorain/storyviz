dae7710fcd6c698c37a8f6566acd0eee2af93a85
demkejon001 <demkejon001@gmail.com>
2020-02-25 10:21:09 -0700

    added maps.py for positioning buildings

---
+++ story_viz/maps.py
@@ -0,0 +1,10 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from scipy.stats import skewnorm
+import math
+
+class MultiAgentPositioningSystem:
+    def __init__(self):
+        self.author = "Dr. Robert Morain"
+        
