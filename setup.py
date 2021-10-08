7161501546efc05812246fc460d6f8448d51d037
Robert Morain <robert.morain@gmail.com>
2021-10-08 09:39:19 -0600

    setup.sh uses pip install

--- setup.py
+++ setup.py
@@ -7,17 +7,7 @@ setup(
     author="Robert Morain",
     author_email="robert.morain@gmail.com",
     packages=['storyviz', 'GDMC'],
-    install_requires=["scipy==1.2.1",
-        "PyOpenGL==3.1.1a1",
-        "numpy==1.16.6",
-        "PyYAML==5.2",
-        "Pillow==6.2.1",
-        "ftputil==3.4",
-        "spacy==2.0.18",
-        "pygame==1.9.6",
-        "xlib==0.21",
-        "matplotlib==2.2.5",
-],
+    install_requires=[],
 )
 # Cythonize GDMC
 from Cython.Build import cythonize                                                      
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pip install scipy==1.2.1 PyOpenGL==3.1.1a1 numpy==1.16.6 PyYAML==5.2 Pillow==6.2.1 ftputil==3.4 spacy==2.0.18 pygame==1.9.6 xlib==0.21 matplotlib==2.2.5
 python setup.py install
 python setup.py build_ext --inplace
 python -m spacy download en_core_web_lg
